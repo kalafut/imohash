6dc134c6a9ad057993474ef68076b2f1c688cb91
William Hatcher <william@hatcher.work>
2025-07-17 23:46:11 -0500

    cmd: add hash checking

--- cmd/imosum/main.go
+++ cmd/imosum/main.go
@@ -3,12 +3,14 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/kalafut/imohash"
 )
@@ -21,30 +23,93 @@ func processFile(path string) {
 	fmt.Printf("%016x  %s\n", hash, path)
 }
 
-func walkDirFunction(path string, dirEntry fs.DirEntry, err error) error {
-   if err != nil {
-      return err
-   }
-   if ! dirEntry.IsDir() {
-      processFile(path)
-   }
-   return nil
+func processWalkFunc(path string, dirEntry fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if !dirEntry.IsDir() {
+		processFile(path)
+	}
+	return nil
 }
 
-func processDir(path string) {
-	if err := filepath.WalkDir(path, walkDirFunction); err != nil {
+func checkFile(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	malformedLines := 0
+	failedHashes := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "  ", 2)
+
+		fmt.Print("parts: ", parts, " len: ", len(parts), "\n")
+
+		if len(parts) != 2 {
+			malformedLines += 1
+			continue
+		}
+
+		hash, err := imohash.SumFile(parts[1])
+		if err != nil {
+			failedHashes += 1
+		}
+		computedHash := fmt.Sprintf("%016x", hash)
+
+		readableResult := "FAILED"
+		if computedHash == parts[0] {
+			readableResult = "OK"
+		} else {
+			failedHashes += 1
+		}
+
+		fmt.Printf("%s: %s\n", parts[1], readableResult)
+	}
+
+	if failedHashes > 1 {
+		fmt.Fprintf(os.Stderr, "imosum: WARNING: %d computed checksums did NOT match\n", failedHashes)
+	} else if failedHashes == 1 {
+		fmt.Fprintf(os.Stderr, "imosum: WARNING: %d computed checksum did NOT match\n", failedHashes)
+	}
+
+	if malformedLines > 1 {
+		fmt.Fprintf(os.Stderr, "imosum: WARNING: %d lines are improperly formatted\n", malformedLines)
+	} else if malformedLines == 1 {
+		fmt.Fprintf(os.Stderr, "imosum: WARNING: %d line is improperly formatted\n", malformedLines)
+	}
+}
+
+func checkWalkFunc(path string, dirEntry fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if !dirEntry.IsDir() {
+		checkFile(path)
+	}
+	return nil
+}
+
+func walkDir(path string, fn fs.WalkDirFunc) {
+	if err := filepath.WalkDir(path, fn); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func main() {
+	chkFlag := flag.Bool("c", false, "read hashes from the FILEs and check them")
 	flag.Parse()
 	paths := flag.Args()
 
 	if len(paths) == 0 {
 		fmt.Println("imosum - Prints the imohash from the file system")
-		fmt.Println("USAGE: imosum path1 path2...")
+		fmt.Println("USAGE: imosum [-c] path1 path2...")
 		fmt.Println("If directories are provided, all files within them will be processed.")
+		fmt.Println("Pass -c to read hashes from the FILEs and check them.")
 		os.Exit(0)
 	}
 
@@ -54,9 +119,17 @@ func main() {
 			log.Fatal(err)
 		}
 		if fileInfo.IsDir() {
-			processDir(path)
+			if *chkFlag {
+				walkDir(path, checkWalkFunc)
+			} else {
+				walkDir(path, processWalkFunc)
+			}
 		} else {
-			processFile(path)
+			if *chkFlag {
+				checkFile(path)
+			} else {
+				processFile(path)
+			}
 		}
 	}
 }
